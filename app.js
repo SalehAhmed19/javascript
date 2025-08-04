383a95cbbb87cfa22581e8b3954acd0ced96456d
SalehAhmed19 <saleh.ahmed.mahin@gmail.com>
2025-08-05 05:41:10 +0600

    task-2

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 // task 1
-let str = "Hello, World!";
-let rStrWord = str.split(" ").reverse().join(" ");
-let rStrLetters = rStrWord.split("").reverse().join("");
+// let str = "Hello, World!";
+// let rStrWord = str.split(" ").reverse().join(" ");
+// let rStrLetters = rStrWord.split("").reverse().join("");
 
-console.log(rStrLetters);
+// console.log(rStrLetters);
+let str = "pop";
+const isPalindrome = (str) => {
+  let rStr = str.split("").reverse().join("");
+  rStr === str
+    ? console.log(str + " is a palindrome")
+    : console.log(str + " is not a palindrome");
+};
+
+isPalindrome(str);
+isPalindrome("str");
