069ec09c88da34dfaf1a0e9dfb82a77bee983ef2
SalehAhmed19 <saleh.ahmed.mahin@gmail.com>
2025-08-05 05:55:34 +0600

    task-3

--- app.js
+++ app.js
@@ -17,14 +17,25 @@
 // isPalindrome(str);
 // isPalindrome("str");
 
-let arr1 = [100, 200, 300];
-let arr2 = [100, 200, 300];
-let arr3 = [100, 200, 200];
-function isEqual(arr1, arr2) {
-  JSON.stringify(arr1) === JSON.stringify(arr2)
-    ? console.log("Arrays are equal")
-    : console.log("Arrays are not equal");
-}
+// let arr1 = [100, 200, 300];
+// let arr2 = [100, 200, 300];
+// let arr3 = [100, 200, 200];
+// function isEqual(arr1, arr2) {
+//   JSON.stringify(arr1) === JSON.stringify(arr2)
+//     ? console.log("Arrays are equal")
+//     : console.log("Arrays are not equal");
+// }
 
-isEqual(arr1, arr2); // Arrays are equal
-isEqual(arr1, arr2); // Arrays are not equal
+// isEqual(arr1, arr2); // Arrays are equal
+// isEqual(arr1, arr2); // Arrays are not equal
+
+console.log(null == undefined); // true
+console.log(null === undefined); // false
+
+/**
+ * The statement console.log(null == undefined); evaluates to true because of how JavaScript's == (loose equality) operator works.
+ *
+ * Loose Equality (==): This operator performs type coercion before comparing the values. If the operands are of different types, JavaScript attempts to convert one or both operands to a common type before making the comparison.
+ *
+ * Strict Equality (===): This operator compares both the value and the type without any type coercion. If the operands are of different types, it will always return false.
+ */
